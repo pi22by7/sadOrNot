7cc3f167d5236d5b03a85c8f493a3da97d3fde53
pi22by7 <piyushairani123@outlook.com>
2023-01-31 21:11:18 +0530

    working ML model and extended dataset

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
+# Load data
+df = pd.read_csv("sad_texts.csv")
+
+# Pre-process text data
+texts = df['text'].str.lower()
+vectorizer = CountVectorizer()
+text_vectors = vectorizer.fit_transform(texts)
+
+# Split data into training and testing sets
+x_train, x_test, y_train, y_test = train_test_split(text_vectors, df['label'], test_size=0.2)
+
+# Train the model
+model = MultinomialNB()
+model.fit(x_train, y_train)
+#
+# # Make predictions on test data
+# y_pred = model.predict(x_test)
+
+input_text = input("Enter a text: ")
+input_features = vectorizer.transform([input_text])
+prediction = model.predict(input_features)
+label = prediction[0]
+confidence = model.predict_proba(input_features)
+
+print("The text is classified as:", "sad" if label == '1' else "not sad")
+print("Confidence", confidence[0][1])
+
+# Evaluate model performance
+# acc = accuracy_score(y_test, y_pred)
+# print("Accuracy:", acc)
--- scrap.py
+++ scrap.py
@@ -3,23 +3,25 @@ from bs4 import BeautifulSoup
 import csv
 
 # Define the URL to scrape
-url = "https://www.invajy.com/sad-quotes/"
+url = "https://twitter.com/search?q=depression"
 
 # Send a GET request to the URL
-response = requests.get(url, headers={"User-Agent": "Roku4640X/DVP-7.70 (297.70E04154A)"})
+response = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, "
+                                                    "like Gecko) Chrome/47.0.2526.111 Safari/537.36"})
 print(response)
 
 # Parse the HTML content of the response
 soup = BeautifulSoup(response.content, "html.parser")
 
 # Find the sad text elements in the HTML
-sad_text_elements = soup.find_all("li")
+sad_text_elements = soup.find_all("div", class_="css-901oao")
 print(sad_text_elements)
 sad_texts = [element.text for element in sad_text_elements]
 
 # Save the sad texts to a CSV file
-with open("sad_texts.csv", "w") as f:
+with open("sad_texts.csv", "a", encoding='utf-8') as f:
     writer = csv.writer(f)
-    writer.writerow(["text", "label"])
+    # writer.writerow(["text", "label"])
+    writer.writerow([])
     for text in sad_texts:
         writer.writerow([text, 1])
