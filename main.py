3cf0feede2d234646a66d4ff0a0217c2107ce511
pi22by7 <piyushairani123@outlook.com>
2023-06-08 23:16:00 +0530

    updated stuffffffffffff

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
 import csv
-
-import pandas as pd
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
-# from sklearn.metrics import accuracy_score
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+# Download necessary NLTK resources
+nltk.download('stopwords')
+nltk.download('wordnet')
 
 # Load data
-df = pd.read_csv("sad_texts.csv")
+df = pd.read_csv("sad_texts.csv", header=None, names=['text', 'label'])
+
+# Convert label column to numeric values
+df['label'] = pd.to_numeric(df['label'], errors='coerce')
+
+# Remove rows with missing label values
+df = df.dropna(subset=['label'])
 
 # Pre-process text data
 texts = df['text'].str.lower()
+
+# Remove special characters and numbers
+texts = texts.apply(lambda x: re.sub(r'[^a-zA-Z]', ' ', x))
+
+# Tokenize the text
+texts = texts.apply(lambda x: x.split())
+
+# Remove stopwords
+stop_words = set(stopwords.words('english'))
+texts = texts.apply(lambda x: [word for word in x if word not in stop_words])
+
+# Lemmatize the words
+lemmatizer = WordNetLemmatizer()
+texts = texts.apply(lambda x: [lemmatizer.lemmatize(word) for word in x])
+
+# Join the words back into sentences
+texts = texts.apply(lambda x: ' '.join(x))
+
+# Vectorize the text data
 vectorizer = CountVectorizer()
 text_vectors = vectorizer.fit_transform(texts)
 
@@ -21,27 +54,50 @@ x_train, x_test, y_train, y_test = train_test_split(text_vectors, df['label'], t
 model = MultinomialNB()
 model.fit(x_train, y_train)
 
-# # Make predictions on test data
-# y_pred = model.predict(x_test)
-
+# Take input from the user
 input_text = input("Enter a text: ")
+
+# Pre-process the input text
+input_text = re.sub(r'[^a-zA-Z]', ' ', input_text)
+input_text = input_text.lower()
+input_text = input_text.split()
+input_text = [word for word in input_text if word not in stop_words]
+input_text = [lemmatizer.lemmatize(word) for word in input_text]
+input_text = ' '.join(input_text)
+
+# Vectorize the input text
 input_features = vectorizer.transform([input_text])
+
+# Make predictions on the input text
 prediction = model.predict(input_features)
-label = prediction[0]
-# print(prediction)
-# print(label)
 confidence = model.predict_proba(input_features)
 
+label = prediction[0]
 print("The text is classified as:", "sad" if label == 1 else "not sad")
-print("Confidence", confidence[0][1])
+print("Confidence:", confidence[0][1] if confidence[0][1]>confidence[0][0] else confidence[0][0])
 
-add = input("was this right? y/n/e  ")
-if add != 'e':
-    with open("sad_texts.csv", "a", encoding='utf-8', newline='\r') as f:
+# Ask for user feedback and update the dataset
+add = input("Was this right? (y/n) ")
+if add.lower() == 'n':
+    with open("sad_texts.csv", "a", encoding='utf-8', newline='\n') as f:
         writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC, quotechar="\"")
-        # writer.writerow([])
-        writer.writerow([str(input_text), label])
+        writer.writerow([input_text, label])
 
-# Evaluate model performance
-# acc = accuracy_score(y_test, y_pred)
-# print("Accuracy:", acc)
+# Plot for training dataset classes
+class_counts = df['label'].value_counts()
+# print(class_counts)
+plt.bar(['Sad', 'Not Sad'], class_counts)
+plt.title('Distribution of Training Dataset Classes')
+plt.xlabel('Emotion')
+plt.ylabel('Count')
+plt.show()
+
+# Plot for confidence scores
+print(confidence)
+confidence = confidence[0]
+print(confidence)
+plt.bar(['Not Sad', 'Sad'], confidence)
+plt.title('Sadness Detection Confidence')
+plt.xlabel('Emotion')
+plt.ylabel('Confidence')
+plt.show()
